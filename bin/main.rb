31be2495b695ee8aa604fad62f11179702188e73
Ahmad Chata <ahmadchata@gmail.com>
2020-08-26 14:48:34 +0100

    Add error count in messages

--- bin/main.rb
+++ bin/main.rb
@@ -9,9 +9,10 @@ file = File.open(ARGV[0]).to_a
 errors = Style.check(file)
 
 if errors.empty?
-	puts Messages::DONE
-  puts Messages::NO_ERRORS
+	print Messages::DONE
+  print Messages::NO_ERRORS
 else
-	puts Messages::DONE
-  errors.each { |error| puts "#{errors.length} found"}
+	print Messages::DONE
+	print "#{errors.length} errors".red
+  errors.each { |error| puts "\n #{error}".red }
 end
--- lib/messages.rb
+++ lib/messages.rb
@@ -1,9 +1,9 @@
 require 'colorize'
 require_relative '../lib/style_rules'
 require_relative '../lib/style'
-require_relative 'main.rb'
+
 
 module Messages
-  DONE = "Linter has finished checking your file and #{errors.length} found".bold
-  NO_ERRORS = 'No errors'.green.bold
+  DONE = "Linter has finished checking your file and found ".bold
+  NO_ERRORS = "No errors".green.bold
 end
